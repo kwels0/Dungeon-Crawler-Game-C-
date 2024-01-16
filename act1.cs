14b547a32028fa7b44080064b4a89b1b4bcd33cb
kwels <150055673+kwels0@users.noreply.github.com>
2024-01-16 13:47:22 +0800

    act1.cs

---
+++ act1.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Game Start");
+        Console.WriteLine ("Game Descrip");
+        
+        Console.WriteLine("What's your name? :");
+        string name = Console.ReadLine();
+        Console.WriteLine("Your name is: " + name);
+        
+        Console.WriteLine ("You start with a sword, armor, and 100 base HP and MP");
+        int x = 100;
+        int y = 100;
+        
+        Console.WriteLine ("Where will you head of to?: ");
+        Console.WriteLine ("The places with quests are Cave, Dungeon, and Forest");
+        string place = Console.ReadLine();
+        
+        string a = "Cave";
+        string b = "Dungeon";
+        string c = "Forest";
+        
+        if (place == "Cave")
+        {
+        Console.WriteLine ("You have decided to go to the " + a); 
+        }
+        
+        else if (place == "Dungeon")
+        {
+        Console.WriteLine ("You have decided to go to the " + b);
+        }
+        
+        else if (place == "Forest")
+        {
+        Console.WriteLine ("You have decided to go to the " + c);
+        }
+        
+    }
+}
